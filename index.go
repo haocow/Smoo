6c08f8b30626708c294d7ccc24f30dac588de67a
Jonathan Hao <jhao19@gmail.com>
2016-07-11 02:32:43 -0700

    [GO] changed output

--- index.go
+++ index.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("/"))
+	fs := http.FileServer(http.Dir("~/go/src/github.com/haocow/Smoo/"))
 	http.Handle("/", fs)
 
 	log.Println("Listening...")
