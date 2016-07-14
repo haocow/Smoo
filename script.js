db077f5ddabfedd6c0f10fef3a50d315f8595ddf
Jonathan Hao <jhao19@gmail.com>
2016-07-13 23:10:26 -0700

    [JHao] added more to test.html

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+// create the module and name it smooApp
+var smooApp = angular.module('smooApp', ['ngRoute']);
+
+// configure routes
+smooApp.config(function($routeProvider) {
+        $routeProvider
+            // route for the home page
+            .when('/', {
+                templateUrl: 'pages/home.html',
+                controller: 'mainController'
+            })
+
+            .when('/add', {
+                 templateUrl: 'pages/add.html',
+                 controller: 'addController'
+            })
+
+            .when('/view', {
+                templateUrl: 'pages/view.html',
+                controller: 'viewController'
+            })
+    }
+);
+
+// create the controller and inject Angular's $scope
+smooApp.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+smooApp.controller('addController', function($scope) {
+    $scope.message = 'Add new transactions here';
+});
+
+smooApp.controller('viewController', function($scope) {
+    $scope.message = 'View your google sheet here';
+});
+
+
+/* FUNCTIONS */
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
